892c0e0fd8202c23d50967adb3398fc4726139a1
manannayak19 <nayakmanan1911@gmail.com>
2025-12-01 13:26:01 -0500

    Implement daily news summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -24,3 +24,16 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
 
     await transporter.sendMail(mailOptions);
 }
+
+export const sendNewsSummaryEmail = async ({ email, date, newsContent }: {email: string, date: string, newsContent: string}): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace('{{date}}', date).replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: `"Signalist" <communication@signalistcom>`,
+        to: email,
+        subject: `Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Signalist`,
+    };
+
+    await transporter.sendMail(mailOptions);
+}
