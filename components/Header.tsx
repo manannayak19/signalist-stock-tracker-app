a42704cad34430a4d070c150e69690f0d327b562
manannayak19 <nayakmanan1911@gmail.com>
2025-12-01 15:46:03 -0500

    Implement search stocks

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
 import UserDropdown from "@/components/UserDropdown";
-import {useRouter} from "next/navigation";
+import {searchStocks} from "@/lib/actions/finhub.actions";
 
-const Header = ({user} : {user: User}) => {
+const Header = async ({ user }: { user: User }) => {
+    const initialStocks = await searchStocks();
 
     return (
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
                 <Link href="/">
-                    <Image src="/assets/icons/logo.svg" alt="Signalist Logo" width={140} height={32} className="h-8 w-auto cursor-pointer"></Image>
+                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
-                {/* navItems*/}
-                    <NavItems />
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
-                <UserDropdown user={user}/>
 
+                <UserDropdown user={user} initialStocks={initialStocks} />
             </div>
         </header>
     )
